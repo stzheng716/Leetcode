74d3b9a9bcd29ddaff0a1063a5ec6c30e6618ae3
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-08-04 13:13:49 -0700

    Time: 390 ms (5%), Space: 52.1 MB (5.05%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -4,28 +4,20 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
-    let availCount = 0;
-    let zeroCount = 0;
-    flowerbed.unshift(0)
-    flowerbed.push(0)
+    let count = 0;
 
-    for(let i = 0; i <= flowerbed.length; i++){
+    flowerbed.unshift(0);
+    flowerbed.push(0);
 
-        if(flowerbed[i] === 0) {
-            zeroCount++;
-        }
-
-        if(flowerbed[i] === 1 || flowerbed[i] === undefined) {
-            if(zeroCount >= 3){
-                availCount++;
-                zeroCount = zeroCount - 3
-                availCount += Math.floor(zeroCount / 2)
-            }
-            zeroCount = 0
+    for(let i = 1; i <= flowerbed.length; i++) {
+        if(flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0 && flowerbed[i] === 0) {
+            flowerbed[i] = 1;
+            count++;
+            console.log("flowerbed", flowerbed)
         }
     }
-
-    return availCount >= n
+    console.log("count", count)
+    return n <= count;
 };
 
 //input flowerbed: numbers[], n: number
@@ -47,3 +39,29 @@ var canPlaceFlowers = function(flowerbed, n) {
         //zeroCount = 0
     
 //return count >= n
+
+
+// var canPlaceFlowers = function(flowerbed, n) {
+//     let availCount = 0;
+//     let zeroCount = 0;
+//     flowerbed.unshift(0)
+//     flowerbed.push(0)
+
+//     for(let i = 0; i <= flowerbed.length; i++){
+
+//         if(flowerbed[i] === 0) {
+//             zeroCount++;
+//         }
+
+//         if(flowerbed[i] === 1 || flowerbed[i] === undefined) {
+//             if(zeroCount >= 3){
+//                 availCount++;
+//                 zeroCount = zeroCount - 3
+//                 availCount += Math.floor(zeroCount / 2)
+//             }
+//             zeroCount = 0
+//         }
+//     }
+
+//     return availCount >= n
+// };
