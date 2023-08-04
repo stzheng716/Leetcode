f1e4716ff06df12a88c3de20dc7e1778f2890466
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-08-04 13:24:59 -0700

    Time: 71 ms (41.54%), Space: 45.7 MB (24.06%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    const numFreq = {};
+    let mostFreqVal = 0;
+    let mostCommonNum = 0;
+
+    for (let num of nums) {
+        numFreq[num] = (numFreq[num] || 0) + 1
+    }
+    
+    for (let num in numFreq) {
+        if (numFreq[num] > mostFreqVal) {
+            mostFreqVal = numFreq[num];
+            mostCommonNum = num
+        }
+    }
+
+    return mostCommonNum;
+};
+
+//use a freq counter
+//init a numFreq = {} 
+//mostFreqVal
+//mostCommonNum;
+
+//for num of nums
+    //numFreq[num] = (numFreq[num] || 0) + 1
+
+//for num of numFreq
+    //if numFreq[num] > mostFreqVal
+        //mostFreqVal = numFreq[num]
+        //mostCommonNum = num
+
+//return mostCommonNum
