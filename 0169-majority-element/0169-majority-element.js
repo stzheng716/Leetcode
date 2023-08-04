9ca8964a081e60d2ba45080da4a314aea5296b02
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-08-04 13:43:48 -0700

    Time: 56 ms (91.96%), Space: 45.3 MB (34.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,18 +3,24 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-    const numFreq = {};
-    let mostFreqVal = 0;
-    let mostCommonNum = 0;
+    let count = 0;
+    let res;
 
-    for (let num of nums) {
-        numFreq[num] = (numFreq[num] || 0) + 1
-        mostCommonNum = numFreq[num] > mostFreqVal ? num : mostCommonNum
-        mostFreqVal = Math.max(mostFreqVal, numFreq[num])
+    for(let num of nums) {
+        if(num !== res) {
+            count--;
+        } else {
+            count++;
+        }
+
+        if(count < 0) {
+            res = num
+            count = 0
+            count++;
+        }
     }
 
-
-    return mostCommonNum;
+    return res;
 };
 
 //use a freq counter
@@ -31,3 +37,20 @@ var majorityElement = function (nums) {
         //mostCommonNum = num
 
 //return mostCommonNum
+
+//O(n) time and O(n) space
+
+// var majorityElement = function (nums) {
+//     const numFreq = {};
+//     let mostFreqVal = 0;
+//     let mostCommonNum = 0;
+
+//     for (let num of nums) {
+//         numFreq[num] = (numFreq[num] || 0) + 1
+//         mostCommonNum = numFreq[num] > mostFreqVal ? num : mostCommonNum
+//         mostFreqVal = Math.max(mostFreqVal, numFreq[num])
+//     }
+
+
+//     return mostCommonNum;
+// };
