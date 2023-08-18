41863adc8b1f4de6925c323d9cea4b589fc68a7b
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-08-17 21:42:40 -0700

    Time: 166 ms (74.43%), Space: 58.4 MB (86.17%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,70 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+
+    nums.sort((a, b) => a - b)
+    const res = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) continue;
+        let currNum = nums[i]
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            if (left === i) {
+                left++
+                continue;
+            }
+
+            if (right === i) {
+                right--;
+                continue;
+            }
+            sum = currNum + nums[left] + nums[right];
+
+            if (sum === 0) {
+                res.push([nums[i], nums[left], nums[right]])
+                right--;
+                left++;
+                while (nums[left] === nums[left - 1] && left < right) {
+                    left++;
+                }
+                while (nums[right] === nums[right + 1] && left < right) {
+                    right--;
+                }
+            }
+            if (sum < 0) {
+                left++
+            } else if (sum > 0) {
+                right--;
+            }
+        }
+    }
+
+    return res;
+};
+
+//input: nums: numbers[]
+//output: numbers[][]
+
+//nums.sort((a,b) => a - b) 
+
+//init a new array as res
+
+//for loop nums
+    //init left
+    //init right
+    //while left < right
+        //sum = i, left, right
+        //if(left === i || right === i)
+            //continue
+        //if(sum === 0)
+            //res.push[nums[i], nums[left], nums[right]]
+        //if(sum < 0)
+            //left++
+        //if(sum > 0)
+            //right--
+
+//return res
