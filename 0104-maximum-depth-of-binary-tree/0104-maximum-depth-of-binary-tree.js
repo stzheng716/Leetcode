0032e1dea9dd7c2bfc2dbf768a3220cc5a3bfea3
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-09-20 19:05:27 -0700

    Time: 53 ms (88.78%), Space: 44.8 MB (83.72%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
+
+//given a root, find the depth of the tree
+
+//if !root return 0 
+
+//count = maxDepth() + 1
+
+//return count
