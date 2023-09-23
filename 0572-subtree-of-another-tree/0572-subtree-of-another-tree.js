a5ed6064984b515dda8907f7e18b9c3b8aa97ddf
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-09-23 10:47:37 -0700

    Time: 71 ms (65.05%), Space: 49.3 MB (49.75%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    if(!root && subRoot) return false
+    if(root && !subRoot) return true
+    
+    if(sameTree(root, subRoot)) {
+       return true
+    }
+
+    return (isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot))
+}
+
+
+function sameTree(root, subRoot) {
+    if(!root && !subRoot) {
+        return true
+    }
+
+    if(root === null && subRoot !== null || root !== null && subRoot === null) {
+        return false;
+    }
+
+    const lSubtree = sameTree(root.left, subRoot.left)
+    const rSubtree = sameTree(root.right, subRoot.right) 
+
+    if(lSubtree && rSubtree && root.val === subRoot.val) {
+        return true
+    }
+
+    return false;
+}
+
+
+//write a function to check if it is the same tree
+//base case
+    //if !root and !subRoot 
+        //return true;
+
+    //if root node === null and subRoot !== null or root node !== null and subRoot === null
+        //return false;
+    
+    //const leftNode = sameTree(root.left, subRoot.left)
+    //const rightNode = sameTree(root.right, subRoot.right)
+
+    //if(leftNode && rightNode && root.val === subRoot.val) {
+        //return true;
+    //}
+
+    //return false;
+
