a5fcf3510f4edf0201890732853932ac02512287
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-09-24 00:28:23 -0700

    Time: 77 ms (65.28%), Space: 49.9 MB (65.89%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function (head) {
+
+    let slow = head;
+    let fast = head.next
+
+    while(fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    let second = slow.next
+    slow.next = null
+    let prev = null;
+
+    while(second){
+        let temp = second.next;
+        second.next = prev;
+        prev = second;
+        second = temp
+    }
+
+    let first = head;
+    second = prev
+
+    while(second) {
+        let temp1 = first.next;
+        let temp2 = second.next;
+        first.next = second
+        second.next = temp1
+        first = temp1;
+        second = temp2
+    }
+};
+
+//fine the middle with fast slow
+//reverse the second half of the list
+
+//merge the list
