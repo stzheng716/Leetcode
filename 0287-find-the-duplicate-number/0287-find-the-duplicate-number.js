644acef584ad973ded094e606f7873d6827e283e
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-09-26 09:23:47 -0700

    Time: 72 ms (76.41%), Space: 49.2 MB (94.02%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -4,11 +4,24 @@
  */
 var findDuplicate = function(nums) {
     
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            if(nums[i] === nums[j]) {
-                return nums[i]
-            }
+    let slow = 0;
+    let fast = 0;
+
+    while(true){
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if(slow === fast) {
+            break;
+        }
+    }
+
+    let slow2 = 0;
+
+    while(true){
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if(slow2 === slow){
+            return slow
         }
     }
     
