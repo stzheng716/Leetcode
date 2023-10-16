05b7ae88a68e1edfb1395c4c67c623b3b38b8e6a
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-10-16 16:29:48 -0700

    Time: 50 ms (63.89%), Space: 41.9 MB (52.73%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -6,22 +6,22 @@ var findMin = function (nums) {
     let l = 0;
     let r = nums.length - 1;
 
-    // let res = nums[0];
+    let res = nums[0];
 
-    while (l < r) {
-        // if(nums[l] < nums[r]) {
-        //     res = Math.min(res, nums[l])
-        //     break;
-        // }
+    while (l <= r) {
+        if(nums[l] < nums[r]) {
+            res = Math.min(res, nums[l])
+            break;
+        }
 
         let m = Math.floor((l + r) / 2)
-        // res = Math.min(res, nums[m])
-        if(nums[m] > nums[r]) {
+        res = Math.min(res, nums[m])
+        if(nums[m] >= nums[l]) {
             l = m + 1
         } else {
-            r = m
+            r = m - 1
         }
     }
 
-    return nums[l]
+    return res;
 };
